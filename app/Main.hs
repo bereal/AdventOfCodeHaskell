ab291918c2b12a2693980dcbc832144814197884
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-11-28 19:05:16 +0200

    Support custom input file

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Client (ClientConfig, initOrReadConfig)
-import Common (DayRunner, Input (HTTPInput))
+import Common (DayRunner, Input (FileInput, HTTPInput))
 import Data.Map ((!))
 import qualified Data.Map as M
 import Data.Maybe (catMaybes, fromMaybe)
@@ -17,9 +17,12 @@ import Options.Applicative
     helper,
     info,
     long,
+    maybeReader,
     metavar,
     option,
+    optional,
     short,
+    strOption,
     value,
     (<**>),
   )
@@ -32,17 +35,19 @@ import qualified Year2021
 
 years = M.fromList [(2019, Year2019.days), (2020, Year2020.days), (2021, Year2021.days)]
 
-run config year day = do
+run config inputPath year day = do
   let yearDays = years ! year
   printf "== Day %02d ==========\n\n" day
   let solve = yearDays !! (day - 1)
-   in solve $ HTTPInput config year day
+      input = maybe (HTTPInput config year day) FileInput inputPath
+   in solve input
   putStrLn "\n===================="
 
 data ParsedFlags = ParsedFlags
   { fBasedir :: Maybe FilePath,
     fYear :: Int,
-    fDay :: Maybe Int
+    fDay :: Maybe Int,
+    fInput :: Maybe String
   }
 
 optionalReader :: Read a => ReadM (Maybe a)
@@ -51,9 +56,12 @@ optionalReader = eitherReader (fmap Just . readEither)
 flagParser :: Parser ParsedFlags
 flagParser =
   ParsedFlags
-    <$> option optionalReader (short 'b' <> long "basedir" <> value Nothing <> metavar "DIRECTORY")
-    <*> option auto (short 'y' <> long "year" <> value 2021 <> metavar "YEAR")
-    <*> option optionalReader (short 'd' <> long "day" <> value Nothing <> metavar "DAY")
+    <$> optional
+      ( strOption (short 'b' <> long "basedir" <> metavar "DIRECTORY")
+      )
+      <*> option auto (short 'y' <> long "year" <> value 2021 <> metavar "YEAR")
+      <*> option optionalReader (short 'd' <> long "day" <> value Nothing <> metavar "DAY")
+      <*> optional (strOption (short 'i' <> long "input" <> metavar "INPUT_FILE"))
 
 parseFlags = do
   args <- getArgs
@@ -61,7 +69,7 @@ parseFlags = do
 
 main = do
   args <- getArgs
-  ParsedFlags {fBasedir, fYear, fDay} <- parseFlags
+  ParsedFlags {fBasedir, fYear, fDay, fInput} <- parseFlags
   config <- initOrReadConfig fBasedir
   let days = maybe [1 .. length (years ! fYear)] (: []) fDay
-  mapM_ (run config fYear) days
+  mapM_ (run config fInput fYear) days
--- src/Common.hs
+++ src/Common.hs
@@ -5,17 +5,19 @@ module Common where
 import Client (ClientConfig, downloadInput)
 import Data.Attoparsec.Text (Parser, parseOnly)
 import Data.Text (Text, pack)
+import qualified Data.Text.IO as IO
 import GHC.Clock (getMonotonicTime)
 import Text.Printf (printf)
 
 data InputParser a = ParsecParser (Parser a) | TextParser (Text -> Either String a) | HardCoded a
 
-data Input = HTTPInput ClientConfig Int Int
+data Input = HTTPInput ClientConfig Int Int | FileInput FilePath
 
 type DayRunner = Input -> IO ()
 
 readInput :: Input -> IO Text
 readInput (HTTPInput config year day) = downloadInput config year day
+readInput (FileInput path) = IO.readFile path
 
 parseInput :: InputParser a -> Text -> a
 parseInput parser input =
@@ -39,4 +41,4 @@ solveDay parser p1 p2 input = do
   let !r2 = p2 v
   end2 <- getMonotonicTime
   printf "Part 1: %s (%fs)\n" (show r1) (end1 - start)
-  printf "Part 2: %s (%fs)\n" (show r2) (end2 - start)
+  printf "Part 2: %s (%fs)\n" (show r2) (end2 - end1)
