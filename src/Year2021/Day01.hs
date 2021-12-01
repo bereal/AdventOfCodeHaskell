6f818feb0beb4cb4b8c8f26e80cd77337e5e8416
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-12-01 07:31:44 +0200

    Solve puzzle 2021/01

--- src/Year2021/Day01.hs
+++ src/Year2021/Day01.hs
@@ -1,9 +1,15 @@
 module Year2021.Day01 where
 
-import Common (Input, InputParser (ParsecParser), skipDay, solveDay)
-import Data.Attoparsec.Text (anyChar, endOfInput, many1)
-import Data.Text (Text)
+import Common (InputParser (ParsecParser), solveDay)
+import Data.Attoparsec.Text (decimal, endOfLine, sepBy1)
 
-parser = ParsecParser $ many1 anyChar
+parser = ParsecParser $ sepBy1 decimal endOfLine
 
-solve = skipDay
+solve1 xs = length $ filter (< 0) $ zipWith (-) xs $ tail xs
+
+solve2 = solve1 . slidingWindow
+  where
+    slidingWindow (x : xs@(y : z : _)) = (x + y + z) : slidingWindow xs
+    slidingWindow _ = []
+
+solve = solveDay parser solve1 solve2
