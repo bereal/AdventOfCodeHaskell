d385f8335cf109d7c83dd1caa1ce3e8178f3bf5f
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-12-02 09:09:48 +0200

    Solve puzzle 2021/02

--- src/Year2021/Day02.hs
+++ src/Year2021/Day02.hs
@@ -1,9 +1,30 @@
 module Year2021.Day02 where
 
-import Common (Input, InputParser (ParsecParser), skipDay, solveDay)
-import Data.Attoparsec.Text (anyChar, endOfInput, many1)
+import Common (Input, InputParser (ParsecParser), solveDay)
+import Control.Applicative
+import Data.Attoparsec.Text (Parser, decimal, endOfLine, sepBy1, space, string)
 import Data.Text (Text)
 
-parser = ParsecParser $ many1 anyChar
+data Command = Depth Int | Forward Int
 
-solve = skipDay
+cmd f name = f <$> (string name *> space *> decimal)
+
+up = cmd (Depth . negate) "up"
+
+down = cmd Depth "down"
+
+forward = cmd Forward "forward"
+
+parser = ParsecParser $ sepBy1 (up <|> down <|> forward) endOfLine
+
+move1 (h, v) (Depth i) = (h, v + i)
+move1 (h, v) (Forward i) = (h + i, v)
+
+solve1 steps = h * v where (h, v) = foldl move1 (0, 0) steps
+
+move2 (h, v, a) (Depth i) = (h, v, a + i)
+move2 (h, v, a) (Forward i) = (h + i, v + a * i, a)
+
+solve2 steps = h * v where (h, v, _) = foldl move2 (0, 0, 0) steps
+
+solve = solveDay parser solve1 solve2
