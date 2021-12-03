043a1c7e06a66fbadf2512c5eedac8307bde483d
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-12-03 13:19:03 +0200

    Simplify 2021/03

--- src/Year2021/Day03.hs
+++ src/Year2021/Day03.hs
@@ -10,32 +10,29 @@ import Data.Text (Text)
 
 parser = ParsecParser $ sepBy1 (many1 $ char '0' $> 0 <|> char '1' $> 1) endOfLine
 
-bitsToInt :: [Int] -> Int
+type Bits = [Int]
+
+bitsToInt :: Bits -> Int
 bitsToInt = foldl1 ((+) . (`shift` 1))
 
+solve1 :: [Bits] -> Int
 solve1 x = rate (> 0) * rate (< 0)
   where
     zeroToMinus1 a = a - fromEnum (a == 0)
     summary = foldl1 (zipWith ((. zeroToMinus1) . (+))) x
     rate f = bitsToInt $ map (fromEnum . f) summary
 
-filterNums _ [x] = x
-filterNums choose nums =
-  let chosen = choose nums
+sieve :: (Int -> Int -> Bool) -> [Bits] -> Bits
+sieve _ [x] = x
+sieve choose nums =
+  let (zeros, ones) = partition ((== 0) . head) nums
+      chosen = if length zeros `choose` length ones then zeros else ones
       bit = head $ head chosen
       subs = map tail chosen
-   in bit : filterNums choose subs
-
-orderBy1stBitPrevalence num =
-  let p@(zs, os) = partition ((== 0) . head) num
-   in if length zs > length os then (os, zs) else p
-
-minority = fst . orderBy1stBitPrevalence
-
-majority = snd . orderBy1stBitPrevalence
+   in bit : sieve choose subs
 
 solve2 xs =
-  let find f = bitsToInt $ filterNums f xs
-   in find majority * find minority
+  let find f = bitsToInt $ sieve f xs
+   in find (<) * find (>)
 
 solve = solveDay parser solve1 solve2
