08b8461be7b1b0b563a61ef6549d4ec27369f60e
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-12-06 10:58:56 +0200

    Solve puzzle 2021/06

--- src/Year2021/Day06.hs
+++ src/Year2021/Day06.hs
@@ -1,9 +1,22 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Year2021.Day06 where
 
-import Common (Input, InputParser (ParsecParser), skipDay, solveDay)
-import Data.Attoparsec.Text (anyChar, endOfInput, many1)
-import Data.Text (Text)
+import Common (Input, InputParser (ParsecParser), solveDay)
+import Data.Attoparsec.Text (char, decimal, sepBy1)
+import Data.List (group, sort)
 
-parser = ParsecParser $ many1 anyChar
+parser = ParsecParser $ sepBy1 decimal $ char ','
 
-solve = skipDay
+next :: [Int] -> [Int]
+next (zeros : rest) =
+  let (prefix, [sevens, eights]) = splitAt 6 rest
+   in prefix ++ [sevens + zeros] ++ [eights, zeros]
+
+initCounter :: [Int] -> [Int]
+initCounter = map (pred . length) . group . sort . (++ [0 .. 8])
+
+run :: Int -> [Int] -> Int
+run n = sum . (!! n) . iterate next . initCounter
+
+solve = solveDay parser (run 80) (run 256)
