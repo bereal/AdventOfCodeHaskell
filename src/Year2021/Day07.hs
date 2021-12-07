0853adabf83a37ca42a960c941a63cd94db554b1
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-12-07 10:22:05 +0200

    Solve puzzle 2021/07

--- src/Year2021/Day07.hs
+++ src/Year2021/Day07.hs
@@ -1,9 +1,20 @@
 module Year2021.Day07 where
 
-import Common (Input, InputParser (ParsecParser), skipDay, solveDay)
-import Data.Attoparsec.Text (anyChar, endOfInput, many1)
-import Data.Text (Text)
+import Common (Input, InputParser (ParsecParser), solveDay)
+import Data.Attoparsec.Text (char, decimal, endOfInput, sepBy1)
 
-parser = ParsecParser $ many1 anyChar
+parser = ParsecParser $ sepBy1 decimal (char ',')
 
-solve = skipDay
+evalFuel :: (Int -> Int -> Int) -> [Int] -> Int -> Int
+evalFuel cost coords target = sum $ map (`cost` target) coords
+
+cost1 a b = abs (a - b)
+
+cost2 a b = let n = abs (a - b) in (n + 1) * n `div` 2
+
+findMin cost coords =
+  let a = minimum coords
+      b = maximum coords
+   in minimum $ map (evalFuel cost coords) [a .. b]
+
+solve = solveDay parser (findMin cost1) (findMin cost2)
