1f5c391dd3634015b0fd24c70d27469770a1576e
Alexander Berezhnoy <alexander.berezhnoy@gmail.com>
2021-12-09 15:56:58 +0200

    Improve solution 2021/09

--- src/Year2021/Day09.hs
+++ src/Year2021/Day09.hs
@@ -1,57 +1,55 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Year2021.Day09 where
 
 import Common (InputParser (ParsecParser), solveDay)
 import Data.Attoparsec.Text (digit, endOfInput, endOfLine, many1, sepBy1)
 import Data.Char (digitToInt)
-import Data.List (sort, sortOn)
+import Data.List (sort)
 import Data.Map ((!))
 import qualified Data.Map as M
 import Data.Set ((\\))
 import qualified Data.Set as S
 
-parser = ParsecParser $ toMap <$> sepBy1 (many1 (digitToInt <$> digit)) endOfLine
+parser = ParsecParser $ sepBy1 (many1 (digitToInt <$> digit)) endOfLine
 
 type Point = (Int, Int)
 
-type Area = M.Map Point Int
+toMap :: [[Int]] -> M.Map Point Int
+toMap as = M.fromList [((row, col), n) | (r, row) <- zip as [0 ..], (n, col) <- zip r [0 ..], n /= 9]
 
-toMap :: [[Int]] -> Area
-toMap as = M.fromList [((row, col), n) | (r, row) <- zip as [0 ..], (n, col) <- zip r [0 ..]]
+toSet :: [[Int]] -> S.Set Point
+toSet = S.fromList . M.keys . toMap
 
-adjacent :: Area -> Point -> [Point]
-adjacent m (x, y) = filter (`M.member` m) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+adjacent :: Point -> [Point]
+adjacent (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
 
-isLocalMinimum :: Area -> Point -> Bool
-isLocalMinimum m p = (m ! p) < minimum (map (m !) $ adjacent m p)
+adjacentInSet s = filter (`S.member` s) . adjacent
 
-findPointBasin :: Area -> Point -> S.Set Point
-findPointBasin m p = find' (S.singleton p) p
-  where
-    find' s p =
-      let adj = S.filter ((/= 9) . (m !)) $ S.fromList (adjacent m p)
-          sub seen n
-            | n `S.member` seen = seen
-            | otherwise = find' (S.insert n seen) n
-       in foldl sub s adj
+adjacentInMap m = filter (`M.member` m) . adjacent
 
-findBasins :: Area -> [S.Set Point]
-findBasins m =
-  let allPoints = S.fromList [p | (p, v) <- M.toList m, v /= 9]
-      find basins points
-        | S.null points = basins
-        | otherwise =
-          let p = S.elemAt 0 points
-              b = findPointBasin m p
-           in find (b : basins) (points \\ b)
-   in find [] allPoints
+isLocalMinimum :: M.Map Point Int -> Point -> Bool
+isLocalMinimum m p = (m ! p) < minimum (map (m !) $ adjacentInMap m p)
 
-solve1 m =
-  let points = filter (isLocalMinimum m) (M.keys m)
+findBasin :: S.Set Point -> S.Set Point
+findBasin (S.null -> True) = S.empty
+findBasin m =
+  let sub seen ((`S.member` seen) -> True) = seen
+      sub seen n = find (S.insert n seen) n
+      find seen p = foldl sub seen $ adjacentInSet m p
+      start = S.elemAt 0 m
+   in find (S.singleton start) start
+
+findAllBasins :: S.Set Point -> [S.Set Point]
+findAllBasins m =
+  let find s = let b = findBasin s in (b : find (s \\ b))
+   in takeWhile (not . S.null) $ find m
+
+solve1 inp =
+  let m = toMap inp
+      points = filter (isLocalMinimum m) (M.keys m)
    in sum $ map ((+ 1) . (m !)) points
 
-solve2 m =
-  let basins = findBasins m
-      top3 = take 3 $ reverse $ sort $ map S.size basins
-   in product top3
+solve2 = product . take 3 . reverse . sort . map S.size . findAllBasins . toSet
 
 solve = solveDay parser solve1 solve2
