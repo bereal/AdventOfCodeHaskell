edd664b2f7257f580b4d41d48c45d4441098aa72
Alexander Berezhnoy <alexander@noice.com>
2022-12-01 11:29:49 +0200

    Simplify solution for 2022/01

--- src/Year2022/Day01.hs
+++ src/Year2022/Day01.hs
@@ -4,11 +4,7 @@ import Common (InputParser (ParsecParser), solveDay)
 import Data.Attoparsec.Text (decimal, endOfLine, sepBy1)
 import Data.List (sort)
 
-parser = ParsecParser $ map sum <$> sepBy1 (sepBy1 decimal endOfLine) (endOfLine *> endOfLine)
+parser :: InputParser [Int]
+parser = ParsecParser $ reverse . sort . map sum <$> sepBy1 (sepBy1 decimal endOfLine) (endOfLine *> endOfLine)
 
-solve1 :: [Int] -> Int
-solve1 = maximum
-
-solve2 = sum . take 3 . reverse . sort
-
-solve = solveDay parser solve1 solve2
+solve = solveDay parser head (sum . take 3)
