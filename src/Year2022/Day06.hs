2fdd56775a126b2e3d6e6255e972ae915378cc5f
Alexander Berezhnoy <alexander@noice.com>
2022-12-06 11:00:11 +0200

    Improve solution for 2022/06

--- src/Year2022/Day06.hs
+++ src/Year2022/Day06.hs
@@ -2,11 +2,12 @@ module Year2022.Day06 where
 
 import Common (InputParser (ParsecParser), solveDay)
 import Data.Attoparsec.Text (many1, letter)
-import Data.List (nub, tails)
+import Data.List (nub, tails, findIndex)
+import Data.Maybe (fromJust)
 
 slide n = map (take n) . tails
 
 solve' :: Int -> String -> Int
-solve' n = fst . head . dropWhile ((/=n) . length . nub . snd) . zip [n..] . slide n
+solve' n = (+n) . fromJust . findIndex ((==n) . length . nub) . slide n
 
 solve = solveDay (ParsecParser $ many1 letter) (solve' 4) (solve' 14)
