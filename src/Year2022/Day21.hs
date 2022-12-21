610a1ffe805917dd8bbd4d97fd9cd4b78dbf578f
Alexander Berezhnoy <alexander@noice.com>
2022-12-21 21:37:52 +0200

    Fix and unify 2022/21

--- src/Year2022/Day21.hs
+++ src/Year2022/Day21.hs
@@ -18,25 +18,16 @@ parser = HM.fromList <$> lineParser line
 
 type NS = HM.HashMap Text Expr
 
-evalOp '*' a b = a * b
-evalOp '/' a b = a `div` b
-evalOp '-' a b = a - b
-evalOp '+' a b = a + b
-
-eval :: Text -> NS -> Int
-eval n ns = case ns ! n of
-    Val i -> i
-    Op left op right -> let
-        lv = eval left ns
-        rv = eval right ns
-        v = evalOp op lv rv
-      in v
+eval '*' a b = a * b
+eval '/' a b = a `div` b
+eval '-' a b = a - b
+eval '+' a b = a + b
 
 data Equation = Const Int | Solver (Int -> Int)
 
 (.>.) g (Solver f) = Solver $ f . g
 
-equation op  (Const a)  (Const b)  = Const $ evalOp op a b
+equation op  (Const a)  (Const b)  = Const $ eval op a b
 equation '*' (Const a)  s          = (`div` a)  .>. s
 equation '*' s          (Const a)  = (`div` a)  .>. s
 equation '/' (Const a)  s          = (a `div`)  .>. s
@@ -48,17 +39,16 @@ equation '-' s          (Const a)  = (a +)      .>. s
 equation '=' (Solver f) (Const a)  = Const $ f a
 equation '=' (Const a)  (Solver f) = Const $ f a
 
-buildEquation :: NS -> Text -> Equation
-buildEquation _ "humn" = Solver id
-buildEquation ns n = case ns ! n of
+buildEquation :: Bool -> NS -> Text -> Equation
+buildEquation True _ "humn" = Solver id
+buildEquation sec ns n = case ns ! n of
     Val i -> Const i
     Op left op right -> let
-        le = buildEquation ns left
-        re = buildEquation ns right
-     in equation op le re
+        le = buildEquation sec ns left
+        re = buildEquation sec ns right
+        op' = if sec && n == "root" then '=' else op
+     in equation op' le re
 
-solve1 = eval "root"
+solve' sec ns = let (Const a) = buildEquation sec ns "root" in a
 
-solve2 ns = let (Const a) = buildEquation ns "root" in a
-
-solve = solveDay parser solve1 solve2
+solve = solveDay parser (solve' False) (solve' True)
